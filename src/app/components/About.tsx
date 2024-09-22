150bd35975d322c49d85b8fade746f21e6fbd0a2
faizaibrahim22 <faizaibrahim14789@gmail.com>
2024-09-22 22:21:35 +0500

    first commit

---
+++ src/app/components/About.tsx
@@ -0,0 +1,9 @@
+function About (){
+    return(
+        <>
+        <h1> I am a developer</h1>
+        </>
+    )
+};
+
+export  default About ;
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import About from "../components/About";
+ export default function home(){
+    return(
+      <>
+      <About/>
+      </>
+    );
+ }; 
+ 
